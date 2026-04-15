9b6c65e00e764348a1d745d037c583fa3a4d4cc5
Kevin Suarez <suarezk453@gmail.com>
2026-04-15 15:30:55 -0500

    feat: Agregar plantilla HTML base para el diseño del sitio web de Vet Animales Felices

--- veterinary_happy_pets/veterinary_happy_pets/settings.py
+++ veterinary_happy_pets/veterinary_happy_pets/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'veterinary_happy_pets.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,4 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
